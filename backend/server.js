7c8f37d7c3f189a90e803403737637f0c9ee5129
shashi <shashiweb64@gmail.com>
2025-07-08 15:35:02 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const myapp = express();
+require('dotenv').config();
+const myport = process.env.SERVERPORT || 9600
 
 myapp.get("/",(req,res)=>{
     res.send("welcome to expess jsooooooooooooooooooopppppppppppppppppp");
 });
 
-myapp.listen(4500,()=>{
-    console.log("app is runing");
+myapp.listen(myport,()=>{
+    console.log(`app is runing: ${myport}`);
 });
 
