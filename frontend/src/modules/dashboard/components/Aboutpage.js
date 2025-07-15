baeffb043f5f0a0883c92f4cf0378e2a5a41a7c1
shashi <shashiweb64@gmail.com>
2025-07-15 15:38:06 +0530

    change

--- frontend/src/modules/dashboard/components/Aboutpage.js
+++ frontend/src/modules/dashboard/components/Aboutpage.js
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from 'react'
+import { apiroot } from '../../../service/Myapi'
+import axios from 'axios'
 
 function Aboutpage() {
-  const [a,b]=useState([]);
+  const [user, updateuser] = useState([]);
 
-  const mylocal = ()=>{
-    b(JSON.parse(localStorage.getItem("mydata")));
+  const getallusers = () => {
+    axios.get(`${apiroot}/alluser`).then((d) => {
+      console.log(d.data);
+      updateuser(d.data);
+    })
   }
 
-  useEffect(()=>{
-    mylocal();
-  },[])
+  useEffect(() => {
+    getallusers();
+  }, [])
+
+
 
   return (
-    <div>Aboutpage
-      <select>
-        {a.map((d)=>{
-          return <option>{d}</option>
-        })}
-      </select>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-12'>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Fullname</th>
+                <th scope="col">Email</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user.map((d) => {
+                return (
+                  <tr>
+                    <th scope="row">{d._id}</th>
+                    <td>{d.fullname}</td>
+                    <td>{d.email}</td>
+                    <td>@mdo</td>
+                  </tr>
+                )
+              })}
 
+
+
+
+            </tbody>
+          </table>
+
+        </div>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/service/Myapi.js
@@ -0,0 +1,7 @@
+
+// local 
+export const apiroot = "http://localhost:5500";
+
+// production  
+// export const apiroot = "";
+
