fbe186c7d0b57b0865ba801a87a8e0d9bce7b989
shashi <shashiweb64@gmail.com>
2025-07-24 15:17:06 +0530

    change back

--- frontend/src/service/Myapi.js
+++ frontend/src/service/Myapi.js
@@ -3,5 +3,5 @@
 // export const apiroot = "http://localhost:5500";
 
 // production  
-export const apiroot = "https://mernproject6week.onrender.com/";
+export const apiroot = "https://mernproject6week.onrender.com";
 
